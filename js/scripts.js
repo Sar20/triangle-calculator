8a87353fb3bc5e57b3c7d082f63e87ab9484f9f4
Student <example@example.com>
2019-09-18 20:53:36 -0700

    create a tool to check a triangle

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("form#triangle").submit(function() {
+    event.preventDefault();
+    var sideA = parseInt($("#sideA").val());
+    var sideB = parseInt($("#sideB").val());
+    var sideC = parseInt($("#sideC").val());
+
+    if ((sideA + sideB > sideC) && (sideA + sideC > sideB) && (sideB + sideC > sideA)) {
+
+      if (sideA == sideB && sideB == sideC) {
+        alert("Equalateral");
+      } else if (((sideA == sideB && sideA != sideC) || (sideA != sideB && sideB == sideC) || (sideA == sideC && sideA && sideC != sideB))) {
+        alert("Isosceles");
+      } else if (sideA != sideB != sideC != sideA) {
+        alert("Scaline");
+      }
+    } else {
+      alert("Not a Triangle! TRY AGAIN!")
+    }
+  });
+});
